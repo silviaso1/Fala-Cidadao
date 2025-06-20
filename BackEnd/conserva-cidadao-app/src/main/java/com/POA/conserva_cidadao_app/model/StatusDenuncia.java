388e2ac3340598b01bb2a4480bfd0412001c9639
Silvia Soares <i06via.so@gmail.com>
2025-06-20 00:44:16 -0300

    resolve conflitos

--- BackEnd/conserva-cidadao-app/src/main/java/com/POA/conserva_cidadao_app/model/StatusDenuncia.java
+++
@@ -1,8 +0,0 @@
-package com.POA.conserva_cidadao_app.model;
-
-public enum StatusDenuncia {
-    DENUNCIADO,
-    EM_ANDAMENTO,
-    RESOLVIDO,
-    INVALIDO
-}
--- BackEnd/conserva-cidadao-app/src/main/java/com/POA/conserva_cidadao_app/repository/DenunciaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.POA.conserva_cidadao_app.repository;
-
-import com.POA.conserva_cidadao_app.model.Denuncia;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface DenunciaRepository extends JpaRepository<Denuncia, Long> {
-    List<Denuncia> findAll();
-}
